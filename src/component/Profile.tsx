d0f7672c50e58618e3912fcd4f7d36e9cdb66bff
Henry Maduekwe <ryhen6931@gmail.com>
2025-01-21 10:29:44 +0100

    changed the tittle  and added socials to the profile

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -6,6 +6,11 @@ export default function Profile(){
             <div className='Profile_card'>
                 <img src={MyProfile} alt="" className='prp' />
                 <h1>Henry Gozy</h1>
+                <div id='socials'>
+                <i className="fa-brands fa-x-twitter"></i>
+                <i className="fa-brands fa-square-github"></i>
+                <i className="fa-brands fa-linkedin"></i>
+                </div>
                 <p className='prp_text'>Frontend Developer | React Enthusiast | lifelong Learner </p>
                 <button className='prp_btn'>contanct me  <i className="fa-solid fa-phone"></i></button>
             </div>
