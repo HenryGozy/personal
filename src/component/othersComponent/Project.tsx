85e9f371ac26cf583165d202d7481fd0b6c76cbd
Henry Maduekwe <ryhen6931@gmail.com>
2025-02-03 18:44:03 +0100

    changed the cart structure of the skills

--- src/component/othersComponent/Project.tsx
+++ src/component/othersComponent/Project.tsx
@@ -1,52 +1,29 @@
-import ProjectImg from "/src/assets/devconnect .png"
-import One from "/src/assets/ONE.png"
-import Shop from "/src/assets/shopingCart.png"
-import todo from "/src/assets/todo.png"
+
 import H2 from "../H2"
+import { ProjectCards } from "../../data"
 export default function Project(){
     return (
         <>
         <div className="Project">
             <H2 title="PROJECT"/>
             <div  className="Project_grid">
-                <div className="Project_card">
-                    <h3>Invideo Ai</h3>
-                    <div className="project_cont">
-                        <img src={One} alt="" />
-                        <div className="PrpC">
-                            <p>DevConnect a platform which let devs to find and connect with one another created using Html | Css | Javascript  <br /> <a href="https://github.com/HenryGozy/invideo-ai" target="_blank" rel="noopener noreferrer">visit code</a></p>
-                        </div>
-                    </div>
-                </div>
-                <div className="Project_card">
-                    <h3>DevConnect</h3>
-                    <div className="project_cont">
-                        <img src={ProjectImg} alt="" />
-                        <div className="PrpC">
-                            <p>DevConnect a platform which let devs to find and connect with one another created using Html | Css | Javascript | React | Typescript <br /> <a href="https://github.com/HenryGozy/DevConnect" target="_blank" rel="noopener noreferrer">view code</a></p>
-                        </div>
-                    </div>
-                </div>
+                {ProjectCards.map((item)=> <div key={item.name} className="Project_card">
+                <h3 className="">{item.name}</h3>
 
-                <div className="Project_card">
-                    <h3>shoping cart</h3>
+                    <img src={item.img} alt="" />
                     <div className="project_cont">
-                        <img src={Shop} alt="" />
+                        <div className="Skills">
+                            <ul>
+                                {item.skills?.map((skill)=> <li>{skill}</li>)}
+                            </ul>
+                        </div>
                         <div className="PrpC">
-                            <p>shoping cart platform  created using Html | Css | Javascript | <br /> <a href="https://github.com/HenryGozy/shop" target="_blank" rel="noopener noreferrer">view code</a></p>
+                            <p>{item.about}</p>
                         </div>
                     </div>
-                </div>
+                </div>) }
+           
 
-                <div className="Project_card">
-                    <h3>shoping cart</h3>
-                    <div className="project_cont">
-                        <img src={todo} alt="" />
-                        <div className="PrpC">
-                            <p>Simple todo app that allow user to create , remove and edit task  created using Html | Css | Javascript | React | Typescript <br /> <a href="https://github.com/HenryGozy/Simple-To-Do-App" target="_blank" rel="noopener noreferrer">view code</a></p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         </>
---
+++ src/data.ts
@@ -0,0 +1,44 @@
+import ProjectImg from "/src/assets/devconnect .png"
+import One from "/src/assets/ONE.png"
+import Shop from "/src/assets/shopingCart.png"
+import todo from "/src/assets/todo.png"
+
+export type Cards = {
+    name:string
+    img:string
+    about:string
+    href:string
+    skills?:string[]
+}
+export const ProjectCards:Cards[] =[
+    {
+        name:"invideo ai",
+        img:One,
+        about:"DevConnect a platform which let devs to find and connect with one another created using Html | Css | Javascript",
+        href:"https://github.com/HenryGozy/invideo-ai",
+        skills:['html',"css",'javascript']
+    },
+    {
+        name:"devconnect",
+        img:ProjectImg,
+        about:"DevConnect a platform which let devs to find and connect with one another created using Html | Css | Javascript",
+        href:"https://github.com/HenryGozy/invideo-ai",
+        skills:['html',"css",'javascript']
+    },
+
+    {
+        name:"simple todo app",
+        img:todo,
+        about:"DevConnect a platform which let devs to find and connect with one another created using Html | Css | Javascript",
+        href:"https://github.com/HenryGozy/invideo-ai",
+        skills:['html',"css",'javascript',"typescript",]
+    },
+
+    {
+        name:"ecommerce platform ",
+        img:Shop,
+        about:"DevConnect a platform which let devs to find and connect with one another created using Html | Css | Javascript",
+        href:"https://github.com/HenryGozy/invideo-ai",
+        skills:['html',"css",'javascript']
+    },
+]
